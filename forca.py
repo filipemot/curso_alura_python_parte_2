452477381746fbfcfb21dab5fedd15f686262baa
Luis Filipe Guedes Mota <lmota@img.com.br>
2021-04-27 18:06:57 -0300

    Trabalhando com Listas

--- forca.py
+++ forca.py
@@ -4,9 +4,11 @@ def jogar():
     print('######################')
 
     palavra_secreta = "banana"
+    letras_acertadas = ["_","_","_","_","_","_"]
     enforcou = False
     acertou = False
 
+    print(letras_acertadas)
     while not enforcou and not acertou:
         chute = input("Qual letra?")
         chute = chute.strip()
@@ -14,10 +16,10 @@ def jogar():
         index = 0
         for letra in palavra_secreta:
             if chute.lower() == letra.lower():
-                print("Encontrei a letra {} na posicao {}".format(letra, index))
+                letras_acertadas[index] = letra
             index = index + 1
 
-        print("jogando...")
+        print(letras_acertadas)
 
     print("Fim do Jogo")
 
