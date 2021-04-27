849d191742ee438ff3948fdd5b54209ba14a363d
Luis Filipe Guedes Mota <lmota@img.com.br>
2021-04-27 18:44:36 -0300

    Trabalhando com List Comprehension

--- README.md
+++ README.md
@@ -7,3 +7,4 @@
  - String
  - List
  - Tuplas
+ - List Comprehension
--- forca.py
+++ forca.py
@@ -3,23 +3,34 @@ def jogar():
     print('Bem vindo ao jogo de forca')
     print('######################')
 
-    palavra_secreta = "banana"
-    letras_acertadas = ["_","_","_","_","_","_"]
+    palavra_secreta = "banana".lower()
+    letras_acertadas = ["_" for _ in palavra_secreta]
     enforcou = False
     acertou = False
+    erros = 0
 
     print(letras_acertadas)
     while not enforcou and not acertou:
         chute = input("Qual letra?")
-        chute = chute.strip()
+        chute = chute.strip().lower()
 
         index = 0
-        for letra in palavra_secreta:
-            if chute.lower() == letra.lower():
-                letras_acertadas[index] = letra
-            index = index + 1
+        if chute in palavra_secreta:
+            for letra in palavra_secreta:
+                if chute == letra:
+                    letras_acertadas[index] = letra
+                index += 1
+        else:
+            erros += + 1
 
+        enforcou = erros == 6
+        acertou = "_" not in letras_acertadas
         print(letras_acertadas)
+
+    if acertou:
+        print("Você ganhou!")
+    else:
+        print("Você perdeu!")
 
     print("Fim do Jogo")
 
