44de3f7e89b01ffc7a852fd31bc12069a99118e6
Luis Filipe Guedes Mota <lmota@img.com.br>
2021-04-27 17:51:06 -0300

    Trabalhando com Strings

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 ## **Implementação de Um jogo de adivinnhação**
 
-**Curso - Python 3 parte 2: Avançando na linguagem* - [hhttps://cursos.alura.com.br/course/python-3-avancando-na-linguagem](https://cursos.alura.com.br/course/python-3-avancando-na-linguagem)
+**Curso - Python 3 parte 2: Avançando na linguagem* - [https://cursos.alura.com.br/course/python-3-avancando-na-linguagem](https://cursos.alura.com.br/course/python-3-avancando-na-linguagem)
 
 **Conceitos Abordados:**
 
- 
+ - String
+ - List
--- forca.py
+++ forca.py
@@ -3,7 +3,24 @@ def jogar():
     print('Bem vindo ao jogo de forca')
     print('######################')
 
+    palavra_secreta = "banana"
+    enforcou = False
+    acertou = False
+
+    while not enforcou and not acertou:
+        chute = input("Qual letra?")
+        chute = chute.strip()
+
+        index = 0
+        for letra in palavra_secreta:
+            if chute.lower() == letra.lower():
+                print("Encontrei a letra {} na posicao {}".format(letra, index))
+            index = index + 1
+
+        print("jogando...")
+
     print("Fim do Jogo")
+
 
 if __name__ == "__main__":
     jogar()
